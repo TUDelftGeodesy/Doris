c4557f4940d081f54ac762396a189395e01a64ae
dlevelt <d.f.levelt@tudelft.nl>
2016-09-29 17:34:15 +0200

    Fixed bug in paralellisation of calc_coordinates

--- sentinel_1/main_code/single_master_stack.py
+++ sentinel_1/main_code/single_master_stack.py
@@ -10,6 +10,8 @@ from resdata import ResData
 from geocode_master import geocode_master
 from ESD_administration import get_BOL_lines, get_f_DC_difference, get_interfero, get_parameter, freadbk, apply_ESD_Nida
 from scipy import linalg
+import resdata
+import collections
 
 from jobs import Jobs
 from dorisparameters import DorisParameters
@@ -1320,23 +1322,97 @@ class SingleMaster(object):
         self.update_res()
 
     def calc_coordinates(self, createdem=False):
-        # Calculate the coordinates of grid cells
+    # Calculate the coordinates of grid cells
 
         dates = [self.stack.keys()[0]]
 
         for date in dates:
+            job_list1 = []
+            job_list2 = []
+
+            doris_dir = self.doris_path
+
             for burst in self.stack[date].keys():
+                path = self.burst_path(date, burst)
+
                 # Create grid coordinates and heights
-                folder = self.burst_path(date,burst)
-                geocode_inputfile = os.path.join(self.input_files, 'input.geocode')
                 if createdem == True:
                     dem_inputfile = os.path.join(self.input_files, 'input.createdem')
                 else:
                     dem_inputfile = False
-                doris_dir = self.doris_path
 
                 # Run if one of the files does not exist...
-                geocode_master(folder, geocode_inputfile, dem_inputfile, doris_dir)
+        ###                geocode_master(folder, geocode_inputfile, dem_inputfile, doris_dir)
+                # this function geocode the bursts of a master file, based on a DEM
+                # Run the create DEM command
+
+                if (not (self.parallel)):
+                    os.chdir(path)
+
+                if not dem_inputfile == False:
+                    if not doris_dir:
+                        command = 'doris ' + dem_inputfile
+                        job_list1.append([path, command])
+                        if (not (self.parallel)):
+                            os.system(command)
+                    else:
+                        command = doris_dir + ' ' + dem_inputfile
+                        job_list1.append([path, command])
+                        if (not (self.parallel)):
+                            os.system(command)
+            if (self.parallel):
+                jobs = Jobs(self.nr_of_jobs)
+                jobs.run(job_list1)
+
+            for burst in self.stack[date].keys():
+                path = self.burst_path(date, burst)
+                os.chdir(path)
+
+                shutil.copy('ifgs.res', 'create_dem.res')
+                resultfile = resdata.ResData(filename='create_dem.res')
+                # Read the .res file
+                resultfile.res_read()
+
+                # Add the slant2height information. This is meant to fake the doris script
+                sl2h_dat = collections.OrderedDict()
+                sl2h_dat['Method'] = 'schwabisch'
+                sl2h_dat['Data_output_file'] = 'dem_radar.raw'
+                sl2h_dat['Data_output_format'] = 'real4'
+                sl2h_dat['First_line (w.r.t. original_master)'] = resultfile.processes['comp_refdem'][
+                    'First_line (w.r.t. original_master)']
+                sl2h_dat['Last_line (w.r.t. original_master)'] = resultfile.processes['comp_refdem'][
+                    'Last_line (w.r.t. original_master)']
+                sl2h_dat['First_pixel (w.r.t. original_master)'] = resultfile.processes['comp_refdem'][
+                    'First_pixel (w.r.t. original_master)']
+                sl2h_dat['Last_pixel (w.r.t. original_master)'] = resultfile.processes['comp_refdem'][
+                    'Last_pixel (w.r.t. original_master)']
+                sl2h_dat['Multilookfactor_azimuth_direction'] = resultfile.processes['comp_refdem'][
+                    'Multilookfactor_azimuth_direction']
+                sl2h_dat['Multilookfactor_range_direction'] = resultfile.processes['comp_refdem'][
+                    'Multilookfactor_range_direction']
+                sl2h_dat['Ellipsoid (name,a,b)'] = 'WGS84 6.37814e+06 6.35675e+06'
+
+                resultfile.insert(sl2h_dat, 'slant2h')
+                resultfile.write()
+
+            for burst in self.stack[date].keys():
+                path = self.burst_path(date,burst)
+                geocode_inputfile = os.path.join(self.input_files, 'input.geocode')
+                # Generate lat / lon files
+                if not doris_dir:
+                    command = 'doris ' + geocode_inputfile
+                    job_list2.append([path, command])
+                    if (not (self.parallel)):
+                        os.system(command)
+                else:
+                    command = doris_dir + ' ' + geocode_inputfile
+                    job_list2.append([path, command])
+                    if (not (self.parallel)):
+                        os.system(command)
+
+            if (self.parallel):
+                jobs = Jobs(self.nr_of_jobs)
+                jobs.run(job_list2)
 
         self.concatenate('phi.raw', 'phi.raw',dt=np.dtype('float32'))
         self.concatenate('lam.raw', 'lam.raw',dt=np.dtype('float32'))
