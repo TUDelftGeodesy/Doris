56aa6e24e91fdb084a7dbb941a97ee1e6aa40d8f
Antonio Valentino <antonio.valentino@tiscali.it>
2020-10-24 13:00:05 +0200

    Improve layout for the doris python packages

--- setup.py
+++ setup.py
@@ -3,7 +3,20 @@ from setuptools import setup
 setup(
     name='doris',
     version='5.0.3',
-    packages=['install', 'doris_stack', 'doris_stack.functions', 'doris_stack.main_code', 'prepare_stack'],
+    packages=[
+        'doris',
+        'doris.doris_stack',
+        'doris.doris_stack.functions',
+        'doris.doris_stack.main_code',
+        'doris.prepare_stack',
+    ],
+    package_dir = {
+        'doris': '',
+        'doris.doris_stack': 'doris_stack',
+        'doris.doris_stack.functions': 'doris_stack/functions',
+        'doris.doris_stack.main_code': 'doris_stack/main_code',
+        'doris.prepare_stack': 'prepare_stack',
+    },
     url='https://github.com/TUDelftGeodesy/Doris',
     license='LICENSE.txt',
     author='Gert Mulder',
